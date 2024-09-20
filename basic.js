286b4fbeda60d07b022538f88fac02d3423edf7c
BhargavRam413 <115223684+BhargavRam413@users.noreply.github.com>
2024-09-20 15:20:27 +0530

    my first commit

---
+++ basic.js
@@ -0,0 +1,22 @@
+// // // A function without name is called Annonimus
+// // let num=[1,2,3,4,5]
+// // num.forEach((ele, i)=> {
+// //     console.log(i)
+// // });
+// // console.log(num);
+
+// /* function parent(a){
+//     a()
+// }
+// parent(child)
+// function child(){
+//     console.log('heloo')
+// }
+//   //call back and higher orderr*/
+
+//   //DOM : it will be tree like structure
+
+// for (let k in document){
+//     console.log('$(k) : $(document)[k]');
+// }
+
